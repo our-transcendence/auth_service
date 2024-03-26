8a7db59e5aea778063d8a1a6eae0c9003510eb13
gd-harco <gd-harco@student.42lyon.fr>
2024-03-26 15:29:17 +0100

    Added basic user models

--- auth/login/models.py
+++ auth/login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+class User(models.Model):
+    login = models.CharField(max_length=15)
+    password = models.CharField(
+        max_length=25,
+        validators=[MinLengthValidator(5, "Must contains at least 5 char")]
+    )
--- auth/login/views.py
+++ auth/login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
+from login.models import User
 
 import json
 
@@ -9,7 +10,19 @@ def login(request):
     if request.method == 'POST':
         string = f'POST method call'
         data = json.loads(request.body)
-        messages.add_message(request, messages.INFO, f'username : {data["username"]}, password : {data["password"]}')
+        username = data["username"]
+        password = data["password"]
+        try:
+            loged = User.objects.get(login=username)
+        except User.DoesNotExist:
+            string = f'User {username} does not exist'
+            return render(request,
+                          'login/login.html',
+                          {"string": string})
+        if loged.password == password:
+            string = f'User {loged.login} authentified'
+        else:
+            string = f'Wrong password for user {loged.login}'
     else:
         string = f'GET method call'
     return render(request,
