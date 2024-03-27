3722c5567a20ff9ce9975a3dd2590d9505a87281
gd-harco <gd-harco@student.42lyon.fr>
2024-03-27 14:55:28 +0100

    Removed awfl code nesting

--- auth/login/endpoint.py
+++ auth/login/endpoint.py
@@ -17,17 +17,18 @@ def return_user_cookie(user):
 
 @csrf_exempt  # TODO: DO NOT USE IN PRODUCTION
 def login(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        username = data["username"]
-        password = data["password"]
-        try:
-            logged = User.objects.get(login=username)
-        except User.DoesNotExist:
-            return response.JsonResponse({'error_type': f'User {username} does not exist'}, status=401)
-        if logged.password == password:
-            return return_user_cookie(logged)
-        else:
-            return response.JsonResponse({'error_type': "bad password"}, status=401)
-    else:
+    if not request.method == 'POST':
         return response.JsonResponse({'error_type': 'This endpoint can only be reached using POST method'}, status=405)
+
+    data = json.loads(request.body)
+    # TODO check JSON Decode
+    username = data["username"]
+    password = data["password"]
+    try:
+        logged = User.objects.get(login=username)
+    except User.DoesNotExist:
+        return response.JsonResponse({'error_type': f'User {username} does not exist'}, status=401)
+    if logged.password == password:
+        return return_user_cookie(logged)
+    else:
+        return response.JsonResponse({'error_type': "bad password"}, status=401)
