2c9299c37cdb32ef98faa45b1f7b42d5b10936ea
gd-harco <gd-harco@student.42lyon.fr>
2024-04-02 16:46:47 +0200

    Added expiration date in token

--- auth/login/endpoint.py
+++ auth/login/endpoint.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.http import response
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST, require_GET
@@ -13,6 +15,8 @@ import jwt
 def return_user_cookie(user):
     user_dict = model_to_dict(user)
     priv = settings.PRIVATE_KEY
+    expdate = datetime.now() + timedelta(minutes=10)
+    user_dict["exp"] = expdate
     payload = jwt.encode(user_dict, priv, algorithm="RS256")
     cookie_response = response.HttpResponse(status=200)
     cookie_response.set_cookie("token", payload, max_age=None)
@@ -28,7 +32,7 @@ def login(request):
     try:
         data = json.loads(request.body)
     except json.JSONDecodeError:
-        return response.JsonResponse({'error_type': "BAD Json Content"}, status=400)
+        return response.HttpResponse(status=400, reason="Bad Json content")
 
     expected_keys = {"username", "password"}
     if set(data.keys()) != expected_keys:
--- auth/login/startup.py
+++ auth/login/startup.py
@@ -1,6 +1,5 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
-import os
 
 from django.conf import settings
 
