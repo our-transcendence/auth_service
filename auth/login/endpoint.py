150bd1456b64e4198a5b6152097eabe4dd19aa93
gd-harco <gd-harco@student.42lyon.fr>
2024-04-03 15:27:57 +0200

    added base logic for "refresh_token" implementation

--- auth/login/endpoint.py
+++ auth/login/endpoint.py
@@ -12,13 +12,12 @@ import json
 import jwt
 
 
-def return_user_cookie(user):
+def return_user_cookie(user, cookie_response):
     user_dict = model_to_dict(user)
     priv = settings.PRIVATE_KEY
     expdate = datetime.now() + timedelta(minutes=10)
     user_dict["exp"] = expdate
     payload = jwt.encode(user_dict, priv, algorithm="RS256")
-    cookie_response = response.HttpResponse(status=200)
     cookie_response.set_cookie("token", payload, max_age=None)
     return cookie_response
 
@@ -27,7 +26,7 @@ def return_user_cookie(user):
 
 
 @csrf_exempt  # TODO: DO NOT USE IN PRODUCTION
-@require_POST
+@require_GET
 def login(request):
     try:
         data = json.loads(request.body)
@@ -47,6 +46,31 @@ def login(request):
         return response.JsonResponse({'error_type': f'User {username} does not exist'}, status=401)
 
     if user.password == password:
-        return return_user_cookie(user)
+        cookie_response = response.JsonResponse({'refresh_token': user.generate_refresh_token()}, status=200)
+        return return_user_cookie(user, cookie_response)
     else:
         return response.JsonResponse({'error_type': "bad password"}, status=401)
+
+
+
+@csrf_exempt # TODO: DO NOT USE IN PRODUCTION
+@require_GET
+def refresh_token(request):
+    try:
+        data = json.loads(request.body)
+    except json.JSONDecodeError:
+        return response.HttpResponse(status=400, reason="Bad Json content")
+
+    expected_key = {"refresh_token"}
+    if set(data.keys()) != expected_key:
+        return response.HttpResponse(status=400, reason="Expected credential not foud")
+
+    token = data["refresh_token"]
+
+    try:
+        payload = jwt.decode(jwt=token, key=settings.PRIVATE_KEY, algorithms=["RS256"])
+        user = User.objects.get(login=payload["user_id"])
+    except (jwt.DecodeError, jwt.ExpiredSignatureError, exceptions.ObjectDoesNotExist):
+        return response.HttpResponse(status=400, reason="Invalid refresh Token")
+
+    return return_user_cookie(user, response.HttpResponse(status=200))
