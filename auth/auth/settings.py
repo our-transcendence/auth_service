fa7dca1dce4da7539b95bc12dd13d057f8f3179a
gd-harco <gd-harco@student.42lyon.fr>
2024-04-18 17:08:31 +0200

    add docker comamnd to up db

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -1,3 +1,5 @@
+#docker run -e POSTGRES_PASSWORD=PASSWORD -e POSTGRES_USER=USER -e POSTGRES_DB=DB -p 5432:5432 postgres
+
 """
 Django settings for auth project.
 
@@ -16,7 +18,6 @@ from ourJWT import OUR_class, OUR_exception
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -82,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'auth.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -117,7 +117,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -128,7 +127,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
