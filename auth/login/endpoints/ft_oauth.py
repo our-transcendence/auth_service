828dc13c80f577988eb76a343baa74be09ee0624
gd-harco <gd-harco@student.42lyon.fr>
2024-05-06 17:01:34 +0200

    started working on cookie instead of body and ourJWT implement

--- auth/login/endpoints/ft_oauth.py
+++ auth/login/endpoints/ft_oauth.py
@@ -5,78 +5,67 @@ import ourJWT.OUR_exception
 # Third-party imports
 import requests
 # Django imports
-from django.http import response, HttpRequest
+from django.http import response, HttpRequest, Http404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST, require_GET
 
 from auth import settings
 # Local application/library specific imports
 from login.models import User
+from ..utils import get_user_from_jwt
 from ..cookie import return_refresh_token
-
 
 @csrf_exempt
 @require_GET
+# return the url to contact when asking for a 42 auth
 def login_42_page(request: HttpRequest):
     return response.HttpResponse(settings.LOGIN_42_PAGE_URL)
 
 
 @csrf_exempt
 @require_GET
-def token_42(request: HttpRequest):
-    try:
-        data = json.loads(request.body)
-    except json.JSONDecodeError:
-        return response.HttpResponseBadRequest(reason="JSON Decode Error")
-
-    expected_keys = {"code"}
-    if set(data.keys()) != expected_keys:
-        return response.HttpResponseBadRequest(reason="Bad Keys")
-    code = data["code"]
+def get_token_42(request: HttpRequest):
+    ft_code = request.COOKIES.get("code")
+    if ft_code is None:
+        return response.HttpResponseBadRequest(reason="no 42 oauth code in request")
 
     post_data = {
         "grant_type": "authorization_code",
         "client_id": settings.API_42_UID,
         "client_secret": settings.API_42_SECRET,
         "redirect_uri": settings.API_42_REDIRECT_URI,
-        "code": code,
+        "code": ft_code,
     }
+
     try:
         oauth_response = requests.post("https://api.intra.42.fr/oauth/token", data=post_data)
     except requests.exceptions.ConnectionError:
-        # TODO: No Error 500
-        return response.HttpResponse(status=500, reason="Cant connect to 42 api")
+        return response.HttpResponse(status=503, reason="Cant connect to 42 api")
     if oauth_response.status_code != 200:
         return response.HttpResponse(status=oauth_response.status_code,
                                      reason=f"Error: {oauth_response.status_code}, {oauth_response.text}")
+
     access_token = oauth_response.json().get("access_token")
-    # TODO: Set access_token has cookie
-    response_content = {"access_token": access_token}
-    return response.JsonResponse(response_content)
+    full_response = response.HttpResponse()
+    full_response.set_cookie("access_token", access_token)
+    return full_response
 
 
 @csrf_exempt
 @require_GET
 def login_42_endpoint(request: HttpRequest):
-    try:
-        data = json.loads(request.body)
-    except json.JSONDecodeError:
-        return response.HttpResponseBadRequest(reason="JSON Decode Error")
-
-    expected_keys = {"access_token"}
-    if set(data.keys()) != expected_keys:
-        return response.HttpResponseBadRequest(reason=f"Bad Keys {data.keys()}, expecting {expected_keys}")
-    access_token = data["access_token"]
+    access_token = request.COOKIES.get("access_token")
+    if access_token is None:
+        return response.HttpResponseBadRequest(reason="no 42 token in request")
 
     login_42, http_error = get_42_login(access_token)
     if login_42 is None:
         return http_error
 
-    # search if login exists in database
-    if not User.objects.filter(login_42=login_42).exists():
+    try:
+        user: User = User.objects.get(login_42__exact=login_42)
+    except User.DoesNotExist:
         return response.HttpResponseBadRequest(reason="There is no account associated with this 42 account")
-        # then just login
-    user = User.objects.filter(login_42=login_42)[0]
     return return_refresh_token(user)
 
 
@@ -85,18 +74,16 @@ def login_42_endpoint(request: HttpRequest):
 @ourJWT.Decoder.check_auth()
 def link_42(request: HttpRequest, **kwargs):
     # check access_token
-    try:
-        data = json.loads(request.body)
-    except json.JSONDecodeError:
-        return response.HttpResponseBadRequest(reason="JSON Decode Error")
-
-    expected_keys = {"access_token"}
-    if set(data.keys()) != expected_keys:
-        return response.HttpResponseBadRequest(reason=f"Bad Keys {data.keys()}, expecting {expected_keys}")
-    access_token = data["access_token"]
+    access_token = request.COOKIES.get("access_token")
+    if access_token is None:
+        return response.HttpResponseBadRequest(reason="no 42 token in request")
 
     # check auth_token for our_transcendence
     # TODO: get auth token from kwargs
+    try:
+        user = get_user_from_jwt(kwargs)
+    except Http404:
+        return response.HttpResponseBadRequest(reason="no user corresponding to auth token")
     try:
         request.COOKIES["auth_token"]
     except KeyError:
