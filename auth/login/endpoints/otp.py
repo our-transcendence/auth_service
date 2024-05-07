9e3576be4397da9a02e15bace182c76aac42531a
gd-harco <gd-harco@student.42lyon.fr>
2024-05-07 15:26:51 +0200

    started splitting otp in subfuction

--- auth/login/endpoints/otp.py
+++ auth/login/endpoints/otp.py
@@ -2,7 +2,7 @@
 from datetime import timedelta
 import json
 
-
+from django.db import IntegrityError, OperationalError
 # Django imports
 from django.http import response, HttpRequest, Http404
 from django.shortcuts import get_object_or_404
@@ -46,52 +46,78 @@ def set_totp(request: HttpRequest, **kwargs):
 @csrf_exempt
 @require_POST
 def otp_submit(request: HttpRequest):
-    auth_token = request.COOKIES.get("auth_token")
-    if auth_token is None:  # pas de token Auth donne, le user n'est pas connecte
-        user_id = request.COOKIES.get("otp_user_ID")
-        if user_id is None:
-            return response.HttpResponseBadRequest()
-        try:
-            user = get_object_or_404(User, pk=user_id)
-        except Http404:
-            return response.HttpResponseNotFound("no user found with given ID")
-    else:
-        try:
-            auth = ourJWT.Decoder.decode(auth_token)
-        except (ourJWT.BadSubject, ourJWT.RefusedToken, ourJWT.ExpiredToken):
-            return response.HttpResponseBadRequest(reason='bad auth token')
-        try:
-            user = get_user_from_jwt({"token": auth})
-        except Http404:
-            return response.HttpResponseNotFound("no user found with given ID")
+    reason = request.COOKIES.get("otp_status")
+    match reason:
+        case "otp_login":
+            return otp_login(request)
+        case "otp_enable":
+            return otp_enable(request)
+        case "otp_disable":
+            return otp_disable(request)
+        case _:
+            return response.HttpResponseBadRequest("no reason given for otp")
 
+@csrf_exempt
+@ourJWT.Decoder.check_auth()
+@require_http_methods(["PATCH"])
+def remove_totp(request: HttpRequest, **kwargs):
+    try:
+        user: User = get_user_from_jwt(kwargs)
+    except Http404:
+        return response.HttpResponseNotFound("no user found with given ID")
+
+    if user.totp_enabled is False:
+        return response.HttpResponseBadRequest("TOTP isn't enabled for given user")
+
+    user.login_attempt = timezone.now()
+    user.save()  # TODO: protect this in all file
+    return response.HttpResponse(status=202, reason="Expecting otp")
+
+
+def otp_login(request: HttpRequest):
+    user_id = request.COOKIES.get("otp_user_ID")
+    if user_id is None:
+        return response.HttpResponseBadRequest()
+    try:
+        user = get_object_or_404(User, pk=user_id)
+    except Http404:
+        return response.HttpResponseNotFound("no user found with given ID")
     if user.totp_key is None:
         return response.HttpResponse(status=412, reason="OTP not set up for user")
+
     if not request.body:
         user.totp_key = None
         user.save()
         return response.HttpResponseBadRequest(reason="empty request")
 
-    user_code = json.loads(request.body).get("otp_code")
-    if user_code is None:
-        if not user.totp_enabled:
-            user.totp_key = None
-            user.save()
+    otp = get_otp_from_body(request.body)
+    if otp is None:
         return response.HttpResponseBadRequest(reason="No otp in request")
-    if (user.login_attempt + timedelta(minutes=1)) < timezone.now():
-        if not user.totp_enabled:
-            user.totp_key = None
-            user.save()
-        return response.HttpResponseForbidden(reason="OTP validation timed out")
+
+    otp_status, otp_response = check_otp(user, otp)
+
+    if otp_status is False:
+        return otp_response
 
     user.login_attempt = None
-    if not user.totp_item.verify(user_code):
+    try:
         user.save()
-        return response.HttpResponseForbidden(reason="Bad OTP")
+    except (IntegrityError, OperationalError) as e:
+        print(e, flush=True)
+        return response.HttpResponse(status=400, reason="Database Failure")
+    return return_refresh_token(user)
 
-    if not user.totp_enabled:
-        user.totp_enabled = True
-        user.save()
-        return response.HttpResponse()
-    user.save()
-    return return_refresh_token(user=user)
+def check_otp(user: User, otp: str):
+    if (user.login_attempt + timedelta(minutes=1)) < timezone.now():
+        return False, response.HttpResponseForbidden(reason="OTP validation timed out")
+    if user.totp_item.verify(otp) is False:
+        return False, response.HttpResponseForbidden(reason="Bad OTP")
+    return True, None
+
+
+def get_otp_from_body(body: HttpRequest.body):
+    try:
+        otp = json.loads(body).get("otp_code")
+    except (json.JSONDecodeError, KeyError):
+        return None
+    return otp
