c6f9ae17715dc8a070343ec423e8190234dc7352
gd-harco (pc portable) <93791134+TheMintest@users.noreply.github.com>
2024-05-23 15:36:03 +0200

    added stats service url

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -171,3 +171,4 @@ API_42_SECRET = os.getenv("API_42_SECRET", default="s-s4t2ud-8a7c6843709eb35d435
 API_42_REDIRECT_URI = os.getenv("API_42_REDIRECT_URI", default="https://127.0.0.1:4443")
 
 USER_SERVICE_URL = os.getenv("USER_SERVICE_URL", "https://user-nginx:4646")
+STATS_SERVICE_URL = os.getenv("STATS_SERVICE_URL", "https://stats-nginx:5151")
