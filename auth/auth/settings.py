dff99e8d561ceac9091b7c3f9846ae9be8691d7d
gd-harco <gd-harco@student.42lyon.fr>
2024-06-19 12:55:29 +0200

    renamed login 42 page in setings.py

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -184,7 +184,7 @@ except KeyError as e:
 
 API_42_REDIRECT_URI = urllib.parse.quote(f"https://{HOST}:4443/intra", safe='')
 
-LOGIN_42_PAGE = f"https://api.intra.42.fr/oauth/authorize?client_id={API_42_UID}&redirect_uri={API_42_REDIRECT_URI}&response_type=code"
+LOGIN_42_PAGE_URL = f"https://api.intra.42.fr/oauth/authorize?client_id={API_42_UID}&redirect_uri={API_42_REDIRECT_URI}&response_type=code"
 
 USER_SERVICE_URL = os.getenv("USER_SERVICE_URL", "https://user-nginx:4646")
 STATS_SERVICE_URL = os.getenv("STATS_SERVICE_URL", "https://stats-nginx:5151")
