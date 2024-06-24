c28c7e785747ed77de189d53b377ea3e12890b06
gd-harco <gd-harco@student.42lyon.fr>
2024-06-24 17:41:37 +0200

    try to re-add display name in jwt

--- auth/login/cookie.py
+++ auth/login/cookie.py
@@ -1,16 +1,18 @@
 # Standard library imports
 import os
 from datetime import datetime, timedelta
-
+import json
 # Django imports
 from django.forms.models import model_to_dict
 from django.http import response
 
 # Local application/library specific imports
+from auth import settings
 from login.models import User
 from . import crypto
 
 # Third-party imports
+import requests
 
 duration = int(os.getenv("AUTH_LIFETIME", "10"))
 
@@ -22,6 +24,7 @@ def return_auth_cookie(user: User, full_response: response.HttpResponse):
                                              "totp_enabled"])
     expdate = datetime.now() + timedelta(minutes=duration)
     user_dict["exp"] = expdate
+    user_dict["display_name"] = get_dn(user.id)
     payload = crypto.encoder.encode(user_dict, "auth")
     full_response.set_cookie(key="auth_token",
                              value=payload,
@@ -42,3 +45,20 @@ def return_refresh_token(user: User):
                              samesite="Strict")
 
     return return_auth_cookie(user, full_response)
+
+def get_dn(id: int):
+    try:
+        response = requests.get(f"{settings.USER_SERVICE_URL}/{id}/infos/",
+                                data=None,
+                                headers=None,
+                                verify=False)
+    except requests.exceptions.ConnectionError as e:
+        print(e, flush=True)
+        return response.HttpResponse(status=408, reason="Cant connect to user-service")
+
+    if response.status_code == 200:
+        print(f"{response.status_code}, {response.reason}", flush=True)
+        return response.HttpResponse(status=response.status_code, reason=response.reason)
+
+    data = response.json()
+    return data["display_name"]
